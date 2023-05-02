5cd0698692f476d9dafcd8958981e322da573aea
VitorValandro <vitormateusd@gmail.com>
2023-05-02 19:09:01 -0300

    feature: setup api server, add user schema, login route and auth middleware

---
+++ api/src/users/users.middleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+const jwtPayloadIsValid = (value: JwtPayload | string): value is JwtPayload => {
+  return (value as JwtPayload).id !== undefined;
+}
+
+export interface AuthorizedRequest extends Request {
+  userId: string
+};
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (!process.env.JWT_SECRET) return res.status(500).json({ message: 'Servidor sem segredo configurado' });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.status(401).json({ message: "Token de autenticação não encontrado" });
+
+  const parts = authHeader.split(" ");
+
+  if (parts.length !== 2)
+    return res.status(401).json({ message: "Token de autenticação corrompido ou mal formatado" });
+
+  const [scheme, token] = parts;
+  const regexValidator = /^Bearer$/i;
+
+  if (!regexValidator.test(scheme))
+    return res.status(401).json({ message: "Token de autenticação corrompido ou mal formatado" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err || !decoded || !jwtPayloadIsValid(decoded)) return res.status(401).send({ message: 'Token de autenticação inválido' });
+
+    (req as AuthorizedRequest).userId = decoded.id;
+    return next();
+  });
+}
---
+++ api/src/users/users.service.ts
@@ -0,0 +1,69 @@
+import { z } from 'zod';
+import { prisma } from '../../prisma-client';
+import bcrypt from 'bcrypt';
+import { Request, Response } from 'express';
+import { Prisma } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+
+export const createUser = async (req: Request, res: Response) => {
+  const UserDto = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string()
+  });
+
+  type CreateUserDto = z.infer<typeof UserDto>;
+
+  const user = req.body as CreateUserDto;
+  try {
+    UserDto.parse(user);
+  }
+  catch (err) {
+    if (err instanceof z.ZodError) return res.status(400).json({ message: 'Formulário inválido' })
+    return res.status(500).json({ error: err })
+  }
+
+  try {
+    user.password = await bcrypt.hash(user.password, 12);
+    const newUser = await prisma.user.create({
+      data: user
+    });
+  }
+  catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError)
+      if (err.code === 'P2002') res.status(400).json({ message: 'Email já cadastrado' })
+
+    return res.status(500).json({ error: err })
+  }
+
+  return res.status(200).json({ message: "Usuário criado com sucesso" })
+}
+
+export const login = async (req: Request, res: Response) => {
+  if (!process.env.JWT_SECRET) return res.status(500).json({ message: 'Servidor sem segredo configurado' });
+  const HOURS_TOKEN_VALIDATION = 8;
+
+  const LoginDto = z.object({
+    email: z.string().email(),
+    password: z.string()
+  });
+  type LoginUserDto = z.infer<typeof LoginDto>;
+
+  const dto = req.body as LoginUserDto;
+  try {
+    LoginDto.parse(dto);
+  }
+  catch (err) {
+    if (err instanceof z.ZodError) return res.status(400).json({ message: 'Formulário inválido' });
+    return res.status(500).json({ error: err });
+  }
+
+  const user = await prisma.user.findUnique({ where: { email: dto.email } });
+
+  if (!user) return res.status(400).json({ message: 'Usuário não encontrado' });
+
+  if (!await bcrypt.compare(dto.password, user.password)) return res.status(400).json({ message: 'Senha incorreta' });
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: HOURS_TOKEN_VALIDATION * 3600 });
+
+  return res.send({ user: { ...user, password: undefined }, token });
+}
