3902d720dfd6c42f43d7b198e121bcb662d3ce20
Vitor Valandro <vitor.rosa@synvia.com>
2023-05-03 22:21:29 -0300

    feature: create a merkle tree implementation

---
+++ blockchain/merkle.ts
@@ -0,0 +1,83 @@
+import { SignTransaction } from "./blockchain";
+import { BinaryLike, createHash } from "crypto";
+
+const sha256 = (data: BinaryLike) => {
+  return createHash('sha256')
+    .update(data)
+    .digest()
+    .toString('hex');
+};
+
+class MerkleNode {
+  public readonly value: string;
+  public readonly left: MerkleNode | null;
+  public readonly right: MerkleNode | null;
+
+  constructor(value: string, left: MerkleNode | null = null, right: MerkleNode | null = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class MerkleTree {
+  public readonly root: MerkleNode;
+  public readonly size: number;
+
+  constructor(root: MerkleNode, size: number) {
+    this.root = root;
+    this.size = size;
+  }
+
+  static create(transactions: SignTransaction[]) {
+    if (!transactions.length) return;
+    const size = Math.ceil(Math.log2(transactions.length)) + 1;
+    const hashedTransactions = transactions.map(t => new MerkleNode(sha256(JSON.stringify(t))));
+    const root = this.makeRoot(hashedTransactions);
+
+    return new MerkleTree(root, size);
+  }
+
+  private static makeRoot(hashedNodes: MerkleNode[]): MerkleNode {
+    if (hashedNodes.length === 1) return hashedNodes[0];
+    const sublist = [];
+
+    for (let i = 0; i < hashedNodes.length; i += 2) {
+      const leftNode = hashedNodes[i];
+      if (i + 1 >= length) {
+        sublist.push(leftNode);
+        break;
+      }
+      const rightNode = hashedNodes[i + 1];
+      const value = leftNode.value + rightNode.value;
+      const node = new MerkleNode(sha256(value), leftNode, rightNode);
+      sublist.push(node);
+    }
+    return this.makeRoot(sublist);
+  }
+
+  private getSiblingOf(hash: string, node: MerkleNode | null = this.root): { node: MerkleNode | null, left?: boolean } | null {
+    if (!node?.value) return null;
+    if (node.value === hash) return { node };
+    if (!node.left && !node.right) return null;
+    if (node.left?.value === hash) return { node: node.right, left: false };
+    if (node.right?.value === hash) return { node: node.left, left: true };
+    return (this.getSiblingOf(hash, node.left) || this.getSiblingOf(hash, node.right));
+  }
+
+  public verify(data: SignTransaction) {
+    let hash = sha256(JSON.stringify(data));
+    let sibling = this.getSiblingOf(hash);
+
+    while (sibling && sibling.node !== null && sibling.node.value !== this.root.value) {
+      const concatenatedHash = sibling.left
+        ? sibling.node.value + hash
+        : hash + sibling.node.value;
+
+      hash = sha256(concatenatedHash);
+      sibling = this.getSiblingOf(hash);
+    }
+
+    return sibling && sibling.node && sibling.node.value === this.root.value ? true : false;
+  }
+}
